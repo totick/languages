0a0da01d64a63c28abcb737626ec86f74dd0f5a3
John Lundgren <john.paul.lundgren@gmail.com>
2022-10-10 18:26:03 +0200

    Firebase config for firestore and hosting

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h2>Hello Languages</h2>
+    </div>
+  );
 }
 
 export default App;
